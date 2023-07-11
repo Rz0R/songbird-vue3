dcf81ef29bd1a45892b7b009ac7265406af233d6
Rz0R <vpa05@mail.ru>
2023-07-11 23:33:48 +1000

    add lang to store

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 
 import App from './App.vue';
 import router from './router';
-import store from './store';
+import { store, key } from './store';
 import './assets/scss/style.scss';
 
-createApp(App).use(router).use(store).mount('#app');
+createApp(App).use(router).use(store, key).mount('#app');
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,32 @@
-import { createStore } from 'vuex';
+import { InjectionKey } from 'vue';
+import { createStore, useStore as baseUseStore, Store } from 'vuex';
 
-export default createStore({
-  state: {},
+import { LANG } from '@/const/common';
+
+type State = {
+  currentLang: LANG;
+};
+
+export const store = createStore<State>({
+  state: {
+    currentLang: LANG.EN,
+  },
   getters: {},
-  mutations: {},
+  mutations: {
+    toggleLang: (state) => {
+      if (state.currentLang === LANG.EN) {
+        state.currentLang = LANG.RU;
+      } else {
+        state.currentLang = LANG.EN;
+      }
+    },
+  },
   actions: {},
   modules: {},
 });
+
+export const key: InjectionKey<Store<State>> = Symbol();
+
+export const useStore = () => {
+  return baseUseStore(key);
+};
