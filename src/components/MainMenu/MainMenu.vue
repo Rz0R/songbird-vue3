1d64583198133b4d479b70a099496d5297908398
Rz0R <vpa05@mail.ru>
2023-07-11 19:59:03 +1000

    fix main menu styles

--- src/components/MainMenu/MainMenu.vue
+++ src/components/MainMenu/MainMenu.vue
@@ -1,9 +1,9 @@
 <template>
   <ul class="home__menu menu-home">
     <li v-for="item in appRoutes" :key="item.path" class="menu-home__item">
-      <router-link :to="item.path" class="menu-home__link">{{
-        item.name.EN
-      }}</router-link>
+      <router-link :to="item.path" class="menu-home__link">
+        {{ item.name.EN }}
+      </router-link>
     </li>
   </ul>
 </template>
