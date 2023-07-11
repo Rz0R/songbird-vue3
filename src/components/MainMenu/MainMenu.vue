c0a1b80525ae055475fb917e7e60889ef3684a62
Rz0R <vpa05@mail.ru>
2023-07-12 00:22:09 +1000

    add store to main menu component

--- src/components/MainMenu/MainMenu.vue
+++ src/components/MainMenu/MainMenu.vue
@@ -2,15 +2,16 @@
   <ul class="menu-home" :class="className">
     <li v-for="item in appRoutes" :key="item.path" class="menu-home__item">
       <router-link :to="item.path" class="menu-home__link">
-        {{ item.name.EN }}
+        {{ item.name[currentLang] }}
       </router-link>
     </li>
   </ul>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent } from 'vue';
 
+import { useStore } from '@/store';
 import { AppRoute } from '@/const/common';
 
 export default defineComponent({
@@ -23,8 +24,12 @@ export default defineComponent({
     const appRoutes = { ...AppRoute };
     Reflect.deleteProperty(appRoutes, 'Main');
 
+    const store = useStore();
+    const currentLang = computed(() => store.state.currentLang);
+
     return {
       appRoutes,
+      currentLang,
     };
   },
 });
