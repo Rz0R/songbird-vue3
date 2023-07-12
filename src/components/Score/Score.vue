d0c837f844b916069a201d847ee1cf988b88a4a9
Rz0R <vpa05@mail.ru>
2023-07-12 19:42:44 +1000

    add translation and game data

--- src/components/Score/Score.vue
+++ src/components/Score/Score.vue
@@ -1,18 +1,27 @@
 <template>
   <h5 class="score">
-    Score: <span class="score__value">{{ score }}</span>
+    {{ TRANSLATION.SCORE[currentLang] }}:
+    <span class="score__value">{{ score }}</span>
   </h5>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { computed, defineComponent } from 'vue';
+
+import { useStore } from '@/store';
+import { TRANSLATION } from '@/const/common';
 
 export default defineComponent({
   setup() {
     const score = 0;
 
+    const store = useStore();
+    const currentLang = computed(() => store.state.currentLang);
+
     return {
       score,
+      currentLang,
+      TRANSLATION,
     };
   },
 });
--- src/const/common.ts
+++ src/const/common.ts
@@ -1,28 +1,60 @@
-export const AppRoute = {
-  Main: {
-    path: '/',
-    name: {
-      EN: 'Main',
-      RU: 'Главная',
-    },
-  },
-  Game: {
-    path: '/game',
-    name: {
-      EN: 'Game',
-      RU: 'Игра',
-    },
-  },
-  Gallery: {
-    path: '/gallery',
-    name: {
-      EN: 'Gallery',
-      RU: 'Галерея',
-    },
-  },
-};
+import { MAX_SCORE } from '@/const/game';
 
 export enum LANG {
   EN = 'EN',
   RU = 'RU',
 }
+
+export const TRANSLATION = {
+  MAIN: {
+    [LANG.EN]: 'Home',
+    [LANG.RU]: 'Главная',
+  },
+  NEW_GAME: {
+    [LANG.RU]: 'Игра',
+    [LANG.EN]: 'Game',
+  },
+  GALLERY: {
+    [LANG.RU]: 'Галерея',
+    [LANG.EN]: 'Gallery',
+  },
+  INSTRUCTION: {
+    [LANG.RU]: ['Послушайте плеер.', 'Выберите птицу из списка.'],
+    [LANG.EN]: ['Listen to the player.', 'Select a bird from the list.'],
+  },
+  NEXT_LEVEL: {
+    [LANG.RU]: 'Следующий уровень',
+    [LANG.EN]: 'Next Level',
+  },
+  SCORE: {
+    [LANG.RU]: 'Счет',
+    [LANG.EN]: 'Score',
+  },
+  CONGRATULATIONS: {
+    [LANG.RU]: 'Поздравляем!',
+    [LANG.EN]: 'Congratulations!',
+  },
+  CONGRATULATORY_MESSAGE: {
+    [LANG.RU]: `Вы прошли викторину и набрали {SCORE} из ${MAX_SCORE} возможных баллов.`,
+    [LANG.EN]: `You passed the quiz and scored {SCORE} out of ${MAX_SCORE} possible points.`,
+  },
+  TRY_AGAIN: {
+    [LANG.RU]: 'Попробовать еще раз!',
+    [LANG.EN]: 'Try again!',
+  },
+};
+
+export const AppRoute = {
+  Main: {
+    path: '/',
+    name: TRANSLATION.MAIN,
+  },
+  Game: {
+    path: '/game',
+    name: TRANSLATION.NEW_GAME,
+  },
+  Gallery: {
+    path: '/gallery',
+    name: TRANSLATION.GALLERY,
+  },
+};
