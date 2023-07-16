99e259f2263e4918aa723d2c03a66e1b54456ca9
Rz0R <vpa05@mail.ru>
2023-07-16 17:54:46 +1000

    add AnswerInstruction component

---
+++ src/components/Answers/AnswerInstruction/AnswerInstruction.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="answers__instructions">
+    <p v-for="(item, idx) in INSTRUCTION[[currentLang]]" :key="idx">
+      {{ item }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue';
+
+import { useStore } from '@/store';
+import { TRANSLATION } from '@/const/common';
+
+export default defineComponent({
+  setup() {
+    const { INSTRUCTION } = TRANSLATION;
+
+    const store = useStore();
+    const currentLang = computed(() => store.state.currentLang);
+
+    return {
+      currentLang,
+      INSTRUCTION,
+    };
+  },
+});
+</script>
--- src/components/Answers/Answers.vue
+++ src/components/Answers/Answers.vue
@@ -1,11 +1,8 @@
 <template>
   <div class="answers">
     <AnswerList :answer-list="[]" />
-    <AnswerDescription :answer-description="birdDescriptions" />
-    <!-- <div class="answers__instructions">
-      <p>Послушайте плеер.</p>
-      <p>Выберите птицу из списка</p>
-    </div> -->
+    <AnswerDescription v-if="false" :answer-description="birdDescriptions" />
+    <AnswerInstruction />
   </div>
 </template>
 
@@ -14,12 +11,14 @@ import { defineComponent } from 'vue';
 
 import AnswerList from './AnswerList';
 import AnswerDescription from './AnswerDescription';
+import AnswerInstruction from './AnswerInstruction';
 import { birdsData } from '@/const/birdsData';
 
 export default defineComponent({
   components: {
     AnswerList,
     AnswerDescription,
+    AnswerInstruction,
   },
   setup() {
     const birdDescriptions = birdsData[1][3];
